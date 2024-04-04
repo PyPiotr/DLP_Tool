8d6670bd310e39307132ada1fc5eddd5df88fb9d
Piotr, Ptaszynski <piotr.ptaszynski@aptiv.com>
2024-04-04 22:48:29 +0200

    feat: admin panel for dlp app

--- dlp_app/admin.py
+++ dlp_app/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from dlp_app import models
 
-# Register your models here.
+
+@admin.register(models.LossPreventionPattern)
+class DataLeakAdmin(admin.ModelAdmin):
+    list_display = ["description", "pattern"]
+
+
+@admin.register(models.DataLeak)
+class LossPreventionPatternAdmin(admin.ModelAdmin):
+    list_display = ["message", "content", "channel", "pattern"]
