8500416680cbc50335f18bd928ecb56456720215
Piotr, Ptaszynski <piotr.ptaszynski@aptiv.com>
2024-04-04 11:01:38 +0200

    fix: wrong port number in sh script

--- start.sh
+++ start.sh
@@ -4,4 +4,4 @@ echo "Migrating the databse..."
 pipenv run python manage.py migrate
 
 echo "Starting the server..."
-pipenv run python manage.py runserver 0.0.0.0:5050
+pipenv run python manage.py runserver 0.0.0.0:8000
