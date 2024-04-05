daaf9be2f3931d4c4b732d53d965dfa84dd4df05
Piotr, Ptaszynski <piotr.ptaszynski@aptiv.com>
2024-04-05 08:11:01 +0200

    feat: create superuser command

--- start.sh
+++ start.sh
@@ -3,5 +3,8 @@
 echo "Migrating the databse..."
 pipenv run python manage.py migrate
 
+echo "superuser"
+pipenv run python manage.py createsu
+
 echo "Starting the server..."
 pipenv run python manage.py runserver 0.0.0.0:8000
